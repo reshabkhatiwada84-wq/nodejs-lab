6c1a1669cba1b9c52a0668775d76c24f27b8a393
reshabkhatiwada84-wq <reshabkhatiwada84@gmail.com>
2026-08-03 21:52:55 +0530

    Lab 01 Submission

---
+++ Lab-01/app.js
@@ -0,0 +1,46 @@
+console.log("Welcome to Node.js");
+
+console.log("Name : Rishabh Khatiwada");
+console.log("Scholar Number : 23145018");
+console.log("Course : BCA");
+console.log("Semester : VII");
+console.log("College : Dev Sanskriti Vishwavidyalaya (DSVV)");
+
+console.log("Hello Node.js");
+console.log("Learning Backend Development");
+console.log("Today's Lab Completed Successfully");
+
+// Variables
+
+const name = "Rishabh Khatiwada";
+const scholarNumber = "23145018";
+const semester = "VII";
+const course = "BCA";
+const college = "Dev Sanskriti Vishwavidyalaya (DSVV)";
+
+console.log("\n------ Variables ------");
+
+console.log("Name :", name);
+console.log("Scholar Number :", scholarNumber);
+console.log("Semester :", semester);
+console.log("Course :", course);
+console.log("College :", college);
+
+
+// =======================
+// Task 7 - Data Types
+// =======================
+
+let student = "Rishabh";
+let age = 21;
+let isStudent = true;
+let city;
+let address = null;
+
+console.log("\n------ Data Types ------");
+
+console.log("student :", typeof student);
+console.log("age :", typeof age);
+console.log("isStudent :", typeof isStudent);
+console.log("city :", typeof city);
+console.log("address :", typeof address);
---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Node.js Lab Assignment 01
+
+## Student Details
+
+- **Name:** Rishabh Khatiwada
+- **Scholar Number:** 23145018
+- **Course:** BCA
+- **Semester:** VII
+- **Subject:** CS403NOD - Node.js
+- **Lab Number:** 01
+- **Date:** 31/07/2026
+- **Node-Version:** screen short on not version 
+
+## Practical Description
+
+This practical demonstrates the basics of Node.js, including installation, project initialization, console output, variables, data types, and the difference between Browser JavaScript and Node.js.
